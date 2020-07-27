2f59c37faea697a35926e1eec6d32d712da3ce74
alithuc <alithu93@gmail.com>
2020-07-26 22:30:03 -0700

    Reading from file instead of URL

--- Codigo Proyecto 1.R
+++ Codigo Proyecto 1.R
@@ -12,4 +12,7 @@ head(best_places_diversity_2017)
 Diversity_2017<-sheets_read("https://docs.google.com/spreadsheets/d/1JXYQnRbevDvJ7ZFBQVXGQrxE2d5LEpP62zBxn1BkOkg/edit#gid=0",sheet="Diversity_2017",skip=2)
 head(Diversity_2017)
 
+#testing making a change and uploading
+
+testingdata<-read.csv("diversity_data.csv")
 
