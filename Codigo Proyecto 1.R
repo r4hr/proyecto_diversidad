ac87c0470f81daca64d93bc1731b4da389e5647d
alithuc <alithu93@gmail.com>
2020-07-26 22:05:28 -0700

    test"

--- Codigo Proyecto 1.R
+++ Codigo Proyecto 1.R
@@ -12,4 +12,6 @@ head(best_places_diversity_2017)
 Diversity_2017<-sheets_read("https://docs.google.com/spreadsheets/d/1JXYQnRbevDvJ7ZFBQVXGQrxE2d5LEpP62zBxn1BkOkg/edit#gid=0",sheet="Diversity_2017",skip=2)
 head(Diversity_2017)
 
+# This is a comment
+
 
