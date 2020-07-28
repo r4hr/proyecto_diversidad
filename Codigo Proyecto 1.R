a1c09d85d3a1781be6d113ba883d4391950e9c2a
alithuc <alithu93@gmail.com>
2020-07-27 21:15:31 -0700

    Avance 27 julio

--- Codigo Proyecto 1.R
+++ Codigo Proyecto 1.R
@@ -10,7 +10,7 @@ library(gargle)
 
 getwd()
 
-diversidad_2017<-read.csv("diversity_data_2017.csv", stringsAsFactors = FALSE)
+diversidad_2017_orig<-read.csv("diversity_data_2017.csv", stringsAsFactors = FALSE)
 glimpse(diversidad_2017)
 
 
@@ -23,5 +23,65 @@ glimpse(ranking_2017)
 colnames(diversidad_2017)
 names(diversidad_2017)
 
-#Cambiar nombre columna name a empresa
-prueba<-rename(diversidad_2017
+#Cambiar nombres a las columnas: name a empresa
+prueba<-rename(diversidad_2017_orig, empresa = name)
+names(prueba)
+
+#Quedarse con los que tienen la data completa 
+prueba<-filter(prueba,data.avail=="Y")
+dim(prueba)
+
+#Quitar columnas de urls
+prueba<-select(prueba,-data.url,-diversity.pg.url)
+dim(prueba)
+
+
+#-T- Leer el diccionario de datos para entender las demas variables
+
+
+# (?!!) Como convertir todos los numeros que estan como chr a int (?) - Me salio solo hacerlo uno por uno asi que solo hice los totales
+str(prueba)
+
+totales<-prueba%>%
+  select(empresa,TOTAL10,FT10,MT10)
+
+head(totales)
+str(totales)
+
+totales$TOTAL10<-as.numeric(as.character(totales$TOTAL10))
+str(totales)
+
+totales$FT10<-as.numeric(as.character(totales$FT10))
+str(totales)
+
+totales$MT10<-as.numeric(as.character(totales$MT10))
+str(totales)
+
+#Encontrar los porcentajes de poblacion femenina por empresa
+p1<-totales$FT10/totales$TOTAL10
+p1
+
+#Agregar el porcentaje de poblacion femenina por empresa al dataset totales
+mutate(totales,p1)
+
+
+ggplot(totales,aes(x=reorder(empresa,p1),y=p1))+
+  geom_col(fill="lightblue3")+
+  theme_minimal()+
+  coord_flip()+
+  geom_text(aes(label=p1))+
+  labs(title="Poblacion Femenina por Empresa",subtitle = "Subtitulo",x="",y="",caption = "Por porcentajes para poder comparar?")
+#Los decimales se ven horrible
+
+#Solo en dos empresas hay mas hombres que mujeres
+
+
+# (?) COMO GUARDAR EL ESTILO EN UN OBJETO? - NO ME SALE :( 
+
+#Ideas y otros: Usar summarise para encontrar los % de hombres y mujeres por "jerarquia" de puestos, por empresa para graficar
+#Que estamos considerando como diversidad?
+#Evaluar por etnicidades tambien
+
+  
+
+
