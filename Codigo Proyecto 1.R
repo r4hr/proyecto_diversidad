a6328b4600a189c7aa5fb7b2972196930167b254
alithuc <alithu93@gmail.com>
2020-07-30 23:53:08 -0700

    Linea13 Importe todos los datos como numericos, agregue todos los totales por etnicidad por sexo a totales y cambie el nombre de p1 a p_femenino

--- Codigo Proyecto 1.R
+++ Codigo Proyecto 1.R
@@ -1,6 +1,6 @@
 
 
-#Desde aqui les voy diciendo sorry por los comentarios realmente tontos. Los he marcado con -T- al principio jajajja 
+#Desde aqui les voy diciendo sorry por los comentarios realmente tontos
 
 library(tidyverse)
 library(ggplot2)
@@ -10,16 +10,19 @@ library(gargle)
 
 getwd()
 
-diversidad_2017_orig<-read.csv("diversity_data_2017.csv", stringsAsFactors = FALSE)
+#Agregando na.strings... leyo los datos como int en lugar de chr :) 
+
+diversidad_2017_orig<-read.csv("diversity_data_2017.csv", stringsAsFactors = FALSE,na.strings = c("n/a","NaN", " "))
 glimpse(diversidad_2017_orig)
+str(diversidad_2017_orig)
 
 
-# -T- Las subi como factor porque es un ranking
+# Las subi como factor porque es un ranking
 ranking_2017<-read.csv("best_places_diversity_2017.csv",stringsAsFactors = TRUE)
 glimpse(ranking_2017)
 
-#Ver y limpiar la base 
-#-T- names y colnames hacen lo mismo? Parece que si
+##### Ver y limpiar la base ####
+#names y colnames hacen lo mismo? Parece que si
 colnames(diversidad_2017_orig)
 names(diversidad_2017_orig)
 
@@ -36,57 +39,48 @@ prueba<-select(prueba,-data.url,-diversity.pg.url)
 dim(prueba)
 
 
-#-T- Leer el diccionario de datos para entender las demas variables
+#Leer el diccionario de datos para entender las demas variables
 
-
-# (?!!) Como convertir todos los numeros que estan como chr a int (?) - Me salio solo hacerlo uno por uno asi que solo hice los totales
-str(prueba)
-
-# Pablo - Probando modificar todas las columnas de chr que son numeros a num
-
-prueba[,7:210] <- sapply(prueba[,7:10], as.numeric) # selecciona las columnas indexadas de la 7 a la 210 y les aplica la funcion as.numeric
-str(prueba)
-
-totales<-prueba%>%
-  select(empresa,TOTAL10,FT10,MT10)
-
-head(totales)
-str(totales)
-
-totales$TOTAL10<-as.numeric(as.character(totales$TOTAL10))
-str(totales)
-
-totales$FT10<-as.numeric(as.character(totales$FT10))
-str(totales)
-
-totales$MT10<-as.numeric(as.character(totales$MT10))
-str(totales)
-
-#Encontrar los porcentajes de poblacion femenina por empresa
+####Encontrar los porcentajes de poblacion femenina por empresa####
 p1<-totales$FT10/totales$TOTAL10
 p1
 
 #Agregar el porcentaje de poblacion femenina por empresa al dataset totales
 mutate(totales,p1)
 
+#Redondearlos a dos decimales
+p1<-round(p1,4)
+p1
 
 ggplot(totales,aes(x=reorder(empresa,p1),y=p1))+
   geom_col(fill="lightblue3")+
   theme_minimal()+
   coord_flip()+
-  geom_text(aes(label=p1))+
+  geom_text(aes(label=p1*100))+
   labs(title="Poblacion Femenina por Empresa",subtitle = "Subtitulo",x="",y="",caption = "Por porcentajes para poder comparar?")
-#Los decimales se ven horrible
-
-#Solo en dos empresas hay mas hombres que mujeres
 
 
-# (?) COMO GUARDAR EL ESTILO EN UN OBJETO? - NO ME SALE :( 
+####Conclusion 1: Solo en dos empresas hay mujeres que hombres: Citigroup y Merck####
 
-#Ideas y otros: Usar summarise para encontrar los % de hombres y mujeres por "jerarquia" de puestos, por empresa para graficar
+####Evaluar la representacion por etnicidades####
+
+
+
+
+
+
+####NOTAS-------------------------------------####
+#Ideas y otros:  encontrar los % de hombres y mujeres por "jerarquia" de puestos, por empresa para graficar
 #Que estamos considerando como diversidad?
 #Evaluar por etnicidades tambien
-
+# (?) COMO GUARDAR EL ESTILO EN UN OBJETO? - NO ME SALE :( 
   
-
-
+#Prueba fallida:
+ggplot(totales) +
+  geom_histogram(aes(x=p1)) +
+  geom_hline(yintercept = 0.5, col = "red") +
+  facet_wrap(~empresa) +
+  labs(title = "Vista de todas las empresas a la vez",
+       subtitle = "Subtitulo",
+       x = "Porcentaje de poblacion",
+       y = "Cantidad")
