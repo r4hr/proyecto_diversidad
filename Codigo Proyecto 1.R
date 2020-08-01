787a181f7a9885c1fbc1352e0263e063a1ab27be
alithuc <alithu93@gmail.com>
2020-07-31 21:13:37 -0700

    Creando nuevo dataframe totales por etnia por empresa e intento de grafico.

--- Codigo Proyecto 1.R
+++ Codigo Proyecto 1.R
@@ -10,7 +10,7 @@ library(gargle)
 
 getwd()
 
-#Agregando na.strings... leyo los datos como int en lugar de chr :) 
+#Agregando na.strings... para que no lea los datos como int en lugar de chr :) 
 
 diversidad_2017_orig<-read.csv("diversity_data_2017.csv", stringsAsFactors = FALSE,na.strings = c("n/a","NaN", " "))
 glimpse(diversidad_2017_orig)
@@ -41,22 +41,26 @@ dim(prueba)
 
 #Leer el diccionario de datos para entender las demas variables
 
+totales<-prueba%>%
+  select(empresa,TOTAL10,FT10,MT10,HISPM10,HISPF10,WHM10,BLKM10,NHOPIM10,ASIANM10,AIANM10,TOMRM10,WHF10,BLKF10,NHOPIF10,ASIANF10,AIANF10,TOMRF10)
+str(totales)
+
 ####Encontrar los porcentajes de poblacion femenina por empresa####
-p1<-totales$FT10/totales$TOTAL10
-p1
+p_femenino<-totales$FT10/totales$TOTAL10
+p_femenino
 
 #Agregar el porcentaje de poblacion femenina por empresa al dataset totales
-mutate(totales,p1)
+mutate(totales,p_femenino)
 
 #Redondearlos a dos decimales
-p1<-round(p1,4)
-p1
+p_femenino<-round(p_femenino,4)
+p_femenino
 
-ggplot(totales,aes(x=reorder(empresa,p1),y=p1))+
+ggplot(totales,aes(x=reorder(empresa,p_femenino),y=p_femenino))+
   geom_col(fill="lightblue3")+
   theme_minimal()+
   coord_flip()+
-  geom_text(aes(label=p1*100))+
+  geom_text(aes(label=p_femenino*100))+
   labs(title="Poblacion Femenina por Empresa",subtitle = "Subtitulo",x="",y="",caption = "Por porcentajes para poder comparar?")
 
 
@@ -65,22 +69,146 @@ ggplot(totales,aes(x=reorder(empresa,p1),y=p1))+
 ####Evaluar la representacion por etnicidades####
 
 
+#### ----------------------------NUEVO NUEVITO NUEVOTE -------------------------------------------####
+
+#Moviendo todas las variables de etnia/sexo a que sean valores de una variable nueva
+diversidad_17_compacto <- totales %>%
+  pivot_longer(cols = -empresa, 
+               names_to = "Etnia_Sexo",
+               values_to = "Cantidad")
+
+diversidad_17_compacto
+
+str(diversidad_17_compacto)
+
+#convertir empresa y Etnia_Sexo a factor (lo tomo como chr antes) --- no se si se puede hacer mas limpio
+#solo para saber los nombres de las variables - Despues se puede borrar
+diversidad_17_compacto$Etnia_Sexo<-as.factor(diversidad_17_compacto$Etnia_Sexo)
+diversidad_17_compacto$empresa<-as.factor(diversidad_17_compacto$empresa)
+str(diversidad_17_compacto)
+
+#ver los valores o "niveles" (levels) que puedes tomar las variables Etnia_Sexo y empresa
+levels(diversidad_17_compacto$Etnia_Sexo)
+levels(diversidad_17_compacto$empresa)
+
+#----- Hasta aqui esta bien :) 
+#De aqui para abajo, no se jajaja
+
+
+#agrupando por etnias
+#regresando los valores a character
+diversidad_17_compacto$Etnia_Sexo<-as.character(diversidad_17_compacto$Etnia_Sexo)
+diversidad_17_compacto$empresa<-as.character(diversidad_17_compacto$empresa)
+str(diversidad_17_compacto)
+
+head(diversidad_17_compacto)
+
+#indian
+indian<-filter(diversidad_17_compacto,Etnia_Sexo=="AIANF10"|Etnia_Sexo=="AIANM10")%>%
+  group_by(empresa)%>%
+  summarise(Indian=sum(Cantidad))#Le puse esto para que la nueva columna/variable se distinga entre etnicidades
+#asian
+asian<-filter(diversidad_17_compacto,Etnia_Sexo=="ASIANF10"|Etnia_Sexo=="ASIANM10")%>%
+  group_by(empresa)%>%
+  summarise(Asian=sum(Cantidad))
+
+#black
+black<-filter(diversidad_17_compacto,Etnia_Sexo=="BLKF10"|Etnia_Sexo=="BLKM10")%>%
+  group_by(empresa)%>%
+  summarise(Black=sum(Cantidad))
+
+#hispanic
+hispanic<-filter(diversidad_17_compacto,Etnia_Sexo=="HISPF10"|Etnia_Sexo=="HISPM10")%>%
+  group_by(empresa)%>%
+  summarise(Hispanic=sum(Cantidad))
+
+#hawaiian
+hawaiian<-filter(diversidad_17_compacto,Etnia_Sexo=="NHOPIF10"|Etnia_Sexo=="NHOPIM10")%>%
+  group_by(empresa)%>%
+  summarise(Hawaiian=sum(Cantidad))%>%
+  replace(., is.na(.), 0)#Porque a la hora de hacer el grafico me salia que habia valores nulos
+
+#two races
+two_races<-filter(diversidad_17_compacto,Etnia_Sexo=="TOMRF10"|Etnia_Sexo=="TOMRM10")%>%
+  group_by(empresa)%>%
+  summarise(Two_races=sum(Cantidad))%>%
+  replace(., is.na(.), 0)
+
+#white
+white<-filter(diversidad_17_compacto,Etnia_Sexo=="WHF10"|Etnia_Sexo=="WHM10")%>%
+  group_by(empresa)%>%
+  summarise(White=sum(Cantidad))
+
+
+#convertir todos a df (se guardaron como tibble)
+as.data.frame(indian)
+as.data.frame(asian)
+as.data.frame(black)
+as.data.frame(hispanic)
+as.data.frame(hawaiian)
+as.data.frame(two_races)
+as.data.frame(white)
+
+#creando un objeto para agregar los totales por etnia por empresa
+totales_etnias_2017<-select(totales,empresa,TOTAL10)
+totales_etnias_2017
+str(totales_etnias_2017)
+
+totales_etnias_2017
+
+#agregar los objetos -- Se puede agregar todos a la vez?
+totales_etnias_2017<-left_join(indian,totales_etnias_2017)%>%
+  as.data.frame(totales_etnias_2017)
+
+totales_etnias_2017<-left_join(asian,totales_etnias_2017)%>%
+  as.data.frame(totales_etnias_2017)
+
+totales_etnias_2017<-left_join(black,totales_etnias_2017)%>%
+  as.data.frame(totales_etnias_2017)
+
+totales_etnias_2017<-left_join(hispanic,totales_etnias_2017)%>%
+  as.data.frame(totales_etnias_2017)
+
+totales_etnias_2017<-left_join(hawaiian,totales_etnias_2017)%>%
+  as.data.frame(totales_etnias_2017)
+
+totales_etnias_2017<-left_join(two_races,totales_etnias_2017)%>%
+  as.data.frame(totales_etnias_2017)
+
+totales_etnias_2017<-left_join(white,totales_etnias_2017)%>%
+  as.data.frame(totales_etnias_2017)
+
+head(totales_etnias_2017)
+
+
+#Bailecito de celebracion por el nuevo dataframe :) 
+#... En realidad falta limpiar los N/A que sean ceros
+
+#Graficando para ver la diversidad (por etnia por empresa) 
+#fill no servia, utilice colour??
+#y alguna otra forma de colorearlo??
+
+ggplot(totales_etnias_2017) +
+  geom_point(colour="blue",aes(x=empresa,y=White/TOTAL10)) +
+  geom_point(colour="red",aes(x=empresa,y=Two_races/TOTAL10)) +
+  geom_point(colour="yellow",aes(x=empresa,y=Hawaiian/TOTAL10)) +
+  geom_point(colour="orange",aes(x=empresa,y=Hispanic/TOTAL10)) +
+  geom_point(colour="green",aes(x=empresa,y=Black/TOTAL10)) +
+  geom_point(colour="purple",aes(x=empresa,y=Asian/TOTAL10)) +
+  geom_point(aes(x=empresa,y=Indian/TOTAL10)) +
+  geom_hline(yintercept = 0.5, col = "red") +
+  coord_flip()+
+  labs(title = "Vista de todas las empresas a la vez",
+       subtitle = "Por etnia",
+       x = "",
+       y = "Porcentaje") #Como mostrarlo como porcentaje??
+
+
+
+#En realidad creo que tendria mas sentido moverlo al reves? Osea poner las etnicidades en lugar de las empresas
 
 
 
 
 ####NOTAS-------------------------------------####
-#Ideas y otros:  encontrar los % de hombres y mujeres por "jerarquia" de puestos, por empresa para graficar
-#Que estamos considerando como diversidad?
-#Evaluar por etnicidades tambien
 # (?) COMO GUARDAR EL ESTILO EN UN OBJETO? - NO ME SALE :( 
-  
-#Prueba fallida:
-ggplot(totales) +
-  geom_histogram(aes(x=p1)) +
-  geom_hline(yintercept = 0.5, col = "red") +
-  facet_wrap(~empresa) +
-  labs(title = "Vista de todas las empresas a la vez",
-       subtitle = "Subtitulo",
-       x = "Porcentaje de poblacion",
-       y = "Cantidad")
